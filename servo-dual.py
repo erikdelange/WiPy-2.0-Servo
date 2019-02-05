c7dce14625ed569e7d221e120a3d471fe0fd9b0a
Erik de Lange <erikdelange@users.noreply.github.com>
2019-02-05 10:12:21 +0100

    Can now handle malformed HTTP requests.

--- servo-dual.py
+++ servo-dual.py
@@ -7,11 +7,11 @@ html = """<!DOCTYPE html>
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<link rel="icon" href="data:;base64,=">
-		<title>WiPy</title>
+		<title> WiPy </title>
 	</head>
 	<body>
 		<div class="container">
-			<h1>Servo</h1>
+			<h1> Servo </h1>
 			<br>
 			<input type="range" id="servo1" value="%(pos1)s" oninput="slider(event)">
 			<p>Position: <span id="position1">%(pos1)s</span></p>
@@ -20,14 +20,15 @@ html = """<!DOCTYPE html>
 			<p>Position: <span id="position2">%(pos2)s</span></p>
 		</div>
 	</body>
-	<script language="javascript">
+	<script>
+		"use strict";
+
 		function slider(event) {
 			var element = event.target;
 			var xmlhttp = new XMLHttpRequest();
 
 			xmlhttp.onreadystatechange = function() {
 				if (this.readyState == 4 && this.status == 200) {
-					//console.log(this.responseText);
 					if (element.id == "servo1")
 						document.getElementById("position1").innerHTML = parseInt(this.responseText);
 					if (element.id == "servo2")
@@ -35,8 +36,7 @@ html = """<!DOCTYPE html>
 				}
 			}
 
-			//console.log("?".concat(element.id, "=", element.value));
-			xmlhttp.open("GET", "?".concat(element.id, "=", element.value), true);
+			xmlhttp.open("GET", "/set?".concat(element.id, "=", element.value), true);
 			xmlhttp.send()
 		}
 	</script>
@@ -51,9 +51,9 @@ class servo:
 
 	def __init__(self, position=50, pwm_channel=None):
 		# min_ and max_ limit a servo's position (initally from 0 to 100)
-		self.min_position = 0
-		self.max_position = 100
-		self.position = position
+		self.min_position = 0  # unit: %
+		self.max_position = 100  # unit: %
+		self.position = position  # unit: %
 		self.pwm_channel = pwm_channel
 		self.move(self.position)
 
@@ -71,46 +71,56 @@ if __name__ == "__main__":
 	from machine import Pin, PWM
 	import socket
 	import url
+	import gc
 
-	# setup Servo
+	# servo setup
 	pwm = PWM(0, frequency=50)
 	servo1 = servo(50, pwm.channel(0, pin=Pin.exp_board.G15))
 	servo2 = servo(50, pwm.channel(1, pin=Pin.exp_board.G16))
+
+	# for demonstration purpose limit servo2's position
 	servo2.min_position = 20
 	servo2.max_position = 80
 
-	# setup Webserver
+	# webserver setup
 	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	serversocket.bind(socket.getaddrinfo('0.0.0.0', 80)[0][-1])
 	serversocket.listen(1)
 
 	while True:
+		gc.collect()
+
 		conn, addr = serversocket.accept()
-		# print("new connection from {0}".format(addr))
-		request = conn.readline()
-		conn.recv(1024)
+		request_line = conn.readline()
+
+		print("request:", request_line, "from", addr[0])
+
+		if request_line in [b"", b"\r\n"]:
+			print("malformed request")
+			conn.close()
+			continue
+
+		while True:
+			line = conn.readline()
+			if line in [b"", b"\r\n"]:
+				break
 
 		conn.sendall("HTTP/1.1 200 OK\nConnection: close\nServer: WiPy\nContent-Type: text/html\n\n")
 
-		# print("request:", request)
+		path = url.path(request_line)
 
-		d = url.extract(request)
+		if path == "/":
+			conn.sendall(html % { "pos1" : str(servo1.position), "pos2" : str(servo2.position)})
 
-		response = None
+		if path == "/set":
+			d = url.query(request_line)
 
-		if "servo1" in d:
-			servo1.move(int(d["servo1"]))
-			response = str(servo1.position)
+			if "servo1" in d:
+				servo1.move(int(d["servo1"]))
+				conn.sendall(str(servo1.position))
+			elif "servo2" in d:
+				servo2.move(int(d["servo2"]))
+				conn.sendall(str(servo2.position))
 
-		if "servo2" in d:
-			servo2.move(int(d["servo2"]))
-			response = str(servo2.position)
-
-		if response is None:
-			# use printf-style formatting to avoid problems with javascript braces
-			response = html % { "pos1" : str(servo1.position), "pos2" : str(servo2.position)}
-
-		conn.send(response)
 		conn.sendall("\n")
 		conn.close()
-		# print("closed connection with {0}".format(addr))
--- servo.py
+++ servo.py
@@ -5,43 +5,45 @@ html = """<!DOCTYPE html>
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<link rel="icon" href="data:;base64,=">
-		<title>WiPy</title>
+		<title> WiPy </title>
 	</head>
 	<body>
 		<div class="container">
-			<h1>Servo</h1>
+			<h1> Servo </h1>
 			<br>
-			<h2>Position</h2>
-			<p>Use the slider to move the servo.</p>
+			<h2> Position </h2>
+			<p> Use the slider to move the servo. </p>
 			<br>
 			<form id="slider">
 				<input type="range" id="position" name="position" value="%d" oninput="oninputEvent(event)">
 			</form>
 			<br><br>
-			<h2>Calibration</h2>
-			<p>Modify field Duty cycle to find the minimum and maximum values for your<br>
+			<h2> Calibration </h2>
+			<p> Modify field Duty cycle to find the minimum and maximum values for your <br>
 			   servo and enter these in fields Minimum - and Maximum duty cycle.
 			<br>
 			<form id="dutycycle data">
 				<input type="number" id="dutycycle" name="dutycycle" value="%.3f" min=0 max=1 step=0.001 oninput="oninputEvent(event)">
-				<label for "dutycycle">Duty cycle</label>
+				<label for "dutycycle"> Duty cycle </label>
 				<br>
 				<input type="number" id="min_dutycycle" name="min_dutycycle" value="%.3f" min=0 max=1 step=0.001 oninput="oninputEvent(event)">
-				<label for "min_dutycycle">Minimum duty cycle</label>
+				<label for "min_dutycycle"> Minimum duty cycle </label>
 				<br>
 				<input type="number" id="max_dutycycle" name="max_dutycycle" value="%.3f" min=0 max=1 step=0.001 oninput="oninputEvent(event)">
-				<label for "max_dutycycle">Maximum duty cycle</label>
+				<label for "max_dutycycle"> Maximum duty cycle </label>
 			</form>
 		</div>
 	</body>
-	<script language="javascript">
+	<script>
+		"use strict";
+
 		function oninputEvent(event) {
 			var element = event.target;
 			var xmlhttp = new XMLHttpRequest();
 
 			xmlhttp.onreadystatechange = function() {
 				if (this.readyState == 4 && this.status == 200) {
-					//console.log(element.id + " oninputEvent: " + this.responseText);
+					// keep the slider and dutycycle field in sync
 					if (element.id == "position")
 						document.getElementById("dutycycle").value = parseFloat(this.responseText);
 					if (element.id == "dutycycle")
@@ -49,8 +51,7 @@ html = """<!DOCTYPE html>
 				}
 			}
 
-			//console.log("?".concat(element.id, "=", element.value));
-			xmlhttp.open("GET", "?".concat(element.id, "=", element.value), true);
+			xmlhttp.open("GET", "set?".concat(element.id, "=", element.value), true);
 			xmlhttp.send()
 		}
 	</script>
@@ -62,9 +63,10 @@ if __name__ == "__main__":
 	from machine import Pin, PWM
 	import socket
 	import url
+	import gc
 
-	# setup for the servo
-	position=50
+	# servo setup
+	position = 50
 	dutycycle = 0.07
 	min_dutycycle = 0.02
 	max_dutycycle = 0.128
@@ -72,50 +74,60 @@ if __name__ == "__main__":
 	pwm = PWM(0, frequency=50)
 	servo = pwm.channel(0, pin=Pin.exp_board.G15, duty_cycle=dutycycle)
 
-	# setup for the webserver
+	# webserver setup
 	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	serversocket.bind(socket.getaddrinfo('0.0.0.0', 80)[0][-1])
 	serversocket.listen()
 
 	while True:
-		conn, addr = serversocket.accept()
+		gc.collect()
 
-		request = conn.readline()
-		conn.recv(1024)
+		conn, addr = serversocket.accept()
+		request_line = conn.readline()
+
+		print("request:", request_line, "from", addr[0])
+
+		if request_line in [b"", b"\r\n"]:
+			print("malformed request")
+			conn.close()
+			continue
+
+		while True:
+			line = conn.readline()
+			if line in [b"", b"\r\n"]:
+				break
 
 		conn.sendall("HTTP/1.1 200 OK\nConnection: close\nServer: WiPy\nContent-Type: text/html\n\n")
 
-		print("request:", request)
+		path = url.path(request_line)
 
-		d = url.extract(request)
-
-		response = None
-
-		if "dutycycle" in d:
-			dutycycle = float(d["dutycycle"])
-			position = int((dutycycle - min_dutycycle) / ((max_dutycycle - min_dutycycle) / 100))
-			servo.duty_cycle(dutycycle)
-			response = "{0}".format(min(max(position,0),100))
-
-		if "position" in d:
-			position = int(d["position"])
-			dutycycle = min_dutycycle + ((max_dutycycle - min_dutycycle) / 100) * position
-			dutycycle = min(max(dutycycle, min_dutycycle), max_dutycycle)
-			servo.duty_cycle(dutycycle)
-			response = "{0:.3f}".format(dutycycle)
-
-		if "min_dutycycle" in d:
-			min_dutycycle = float(d["min_dutycycle"])
-			response = "{0:.3f}".format(min_dutycycle)
-
-		if "max_dutycycle" in d:
-			max_dutycycle = float(d["max_dutycycle"])
-			response = "{0:.3f}".format(max_dutycycle)
-
-		if response is None:
+		if path == "/":
 			# use printf-style formatting to avoid problems with javascript braces
-			response = html % (position, dutycycle, min_dutycycle, max_dutycycle)
+			conn.sendall(html % (position, dutycycle, min_dutycycle, max_dutycycle))
 
-		conn.send(response)
+		if path == "/set":
+			d = url.query(request_line)
+
+			if "dutycycle" in d:
+				dutycycle = float(d["dutycycle"])
+				position = int((dutycycle - min_dutycycle) / ((max_dutycycle - min_dutycycle) / 100))
+				servo.duty_cycle(dutycycle)
+				conn.sendall("{}".format(min(max(position,0),100)))
+
+			if "position" in d:
+				position = int(d["position"])
+				dutycycle = min_dutycycle + ((max_dutycycle - min_dutycycle) / 100) * position
+				dutycycle = min(max(dutycycle, min_dutycycle), max_dutycycle)
+				servo.duty_cycle(dutycycle)
+				conn.sendall("{0:.3f}".format(dutycycle))
+
+			if "min_dutycycle" in d:
+				min_dutycycle = float(d["min_dutycycle"])
+				conn.sendall("{0:.3f}".format(min_dutycycle))
+
+			if "max_dutycycle" in d:
+				max_dutycycle = float(d["max_dutycycle"])
+				conn.sendall("{0:.3f}".format(max_dutycycle))
+
 		conn.sendall("\n")
 		conn.close()
